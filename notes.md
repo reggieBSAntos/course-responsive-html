b1d38c181531e024e948d4eb306c522d0ce0bd6f
reggieBSAntos <regginaldsantos@gmail.com>
2025-01-16 06:35:56 +0800

    Initial commit

---
+++ notes.md
@@ -0,0 +1,13 @@
+# TIPS
+
+1. Follow Golden Ratio (62% 38% or 1.68..) in layouts and font-sizing (for single liner)
+2. Use line height instead of x-margin for adjacent text elements
+3. Plan schemantics carefully
+4. Create usable classes
+5. ../ mean go a folder up in CSS
+6. Different typeface for headings and body
+7. Headings typeface bold, minimal line spacing, preferrably solid fonts
+8. focus effect same as hover
+9. declare and use variables in sizing variable, adjust variables in media query
+10. only use "em" in sizing media query
+11. inline element are affected by "text-align" CSS
---
+++ script.js
@@ -0,0 +1,88 @@
+/* SET CURRENT YEAR IN COPYRIGHT */
+const yearEl = (document.querySelector(".year").textContent =
+  new Date().getFullYear());
+
+/* MOBILE NAVIGATION */
+const bntNav = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+
+bntNav.addEventListener("click", (e) => {
+  e.preventDefault();
+  // e.target.closest("button");
+  headerEl.classList.toggle("nav-open");
+});
+///////////////////////////////////////////////////////////
+// Smooth scrolling animation
+const linksEls = document.querySelectorAll("a:link");
+
+Array.from(linksEls).forEach((link) => {
+  link.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    const href = link.getAttribute("href");
+    const a = e.target.closest("a");
+    if (href === "#") {
+      window.scrollTo({ top: 0, behavior: "smooth" });
+      return;
+    }
+
+    if (a.classList.contains("main-nav-link")) {
+      headerEl.classList.toggle("nav-open");
+    }
+
+    if (href !== "#" && href.startsWith("#")) {
+      document.querySelector(href).scrollIntoView({ behavior: "smooth" });
+      return;
+    }
+  });
+});
+
+///////////////////////////////////////////////////////////
+// Sticky Navigation
+
+const sectionHeroEl = document.querySelector(".section-hero");
+
+const obs = new IntersectionObserver(
+  (entries) => {
+    const ent = entries[0];
+    if (!ent.isIntersecting) {
+      document.body.classList.toggle("sticky", true);
+      return;
+    }
+
+    if (ent.isIntersecting) {
+      document.body.classList.toggle("sticky", false);
+      return;
+    }
+  },
+  {
+    // in the viewport
+    root: null,
+    threshold: 0,
+    rootMargin: "-59px",
+  }
+);
+
+obs.observe(sectionHeroEl);
+
+///////////////////////////////////////////////////////////
+// Fixing flexbox gap property missing in some Safari versions
+function checkFlexGap() {
+  var flex = document.createElement("div");
+  flex.style.display = "flex";
+  flex.style.flexDirection = "column";
+  flex.style.rowGap = "1px";
+
+  flex.appendChild(document.createElement("div"));
+  flex.appendChild(document.createElement("div"));
+
+  document.body.appendChild(flex);
+  var isSupported = flex.scrollHeight === 1;
+  flex.parentNode.removeChild(flex);
+  console.log(isSupported);
+
+  if (!isSupported) document.body.classList.add("no-flexbox-gap");
+}
+checkFlexGap();
+
+// https://unpkg.com/smoothscroll-polyfill@0.4.4/dist/smoothscroll.min.js
